38d76b00a38e48aa2cf19ce006614c2ce79b3e6c
SergioTrac <46674106+SergioTrac@users.noreply.github.com>
2026-02-04 16:01:00 +0100

    test(utils): Created more test for the Vector2 struct.

--- tests/DinoLife.Core.Tests/VectorTests.cs
+++ tests/DinoLife.Core.Tests/VectorTests.cs
@@ -23,4 +23,73 @@ public class VectorTests()
 
         (vector.Normalized()==vectorNorm).Should().BeTrue();
     }
+
+    [Fact]
+    public void Multiply_By_Scalar_Should_Scale_Vector()
+    {
+        var vector = new Vector2(2, 3);
+
+        Vector2 result = vector * 2f;
+
+        result.X.Should().Be(4f);
+        result.Y.Should().Be(6f);
+    }
+
+    [Fact]
+    public void Multiply_Scalar_By_Vector_Should_Scale_Vector()
+    {
+        var vector = new Vector2(2, 3);
+
+        Vector2 result = 2f * vector;
+
+        result.Should().Be(new Vector2(4, 6));
+    }
+
+    [Fact]
+    public void Magnitude_Should_Be_Correct()
+    {
+        var vector = new Vector2(3, 4);
+
+        float magnitude = vector.Magnitude();
+
+        magnitude.Should().Be(5f);
+    }
+
+    [Fact]
+    public void Normalizing_Zero_Vector_Should_Return_Zero_Vector()
+    {
+        var vector = new Vector2(0, 0);
+
+        Vector2 normalized = vector.Normalized();
+
+        normalized.Should().Be(new Vector2(0, 0));
+    }
+
+    [Fact]
+    public void Equality_Operator_Should_Return_True_For_Same_Values()
+    {
+        var a = new Vector2(1.5f, 2.5f);
+        var b = new Vector2(1.5f, 2.5f);
+
+        (a == b).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Inequality_Operator_Should_Return_True_For_Different_Values()
+    {
+        var a = new Vector2(1, 2);
+        var b = new Vector2(2, 1);
+
+        (a != b).Should().BeTrue();
+    }
+
+    [Fact]
+    public void Equals_Should_Match_Operator_Equality()
+    {
+        var a = new Vector2(5, 5);
+        var b = new Vector2(5, 5);
+
+        a.Equals(b).Should().BeTrue();
+    }
+
 }
